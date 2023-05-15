e6e891e4f79f534121deed1770a3af60e33a6846
txtahhh56 <boiko.alex56@yandex.ru>
2023-05-16 01:58:48 +0300

    Добавлены эндпоинты Жени Ч.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,8 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import (
-    CategoryViewSet,
-    GenreViewSet,
-    TitleViewSet,
-    CommentViewSet,
-    ReviewViewSet,
-)
+from .views import (CategoryViewSet, CommentViewSet, GenreViewSet, GetToken,
+                    ReviewViewSet, Signup, TitleViewSet, UserViewSet)
 
 app_name = 'api'
 
@@ -21,7 +16,10 @@ router.register(r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet,
 router.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet, basename='comments')
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
+    path('v1/auth/signup/', Signup),
+    path('v1/auth/token/', GetToken),
 ]
