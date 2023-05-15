502fffb3c4b0493127a8401dc8c2f856c9b3316e
txtahhh56 <boiko.alex56@yandex.ru>
2023-05-16 02:09:20 +0300

    Добавлен функционал Жени Ч.

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,17 +1,30 @@
-from rest_framework import filters, mixins, viewsets, serializers
-from django.db.models import Avg
+
 from django.db import IntegrityError
+from django.db.models import Avg
+
+
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
 from django_filters.rest_framework import DjangoFilterBackend
 from django.shortcuts import get_object_or_404
 
-from reviews.models import Category, Genre, Title, Review
+from rest_framework import filters, mixins, viewsets, status, serializers
+from rest_framework.decorators import action, api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import AccessToken
+
+from reviews.models import Category, Genre, Review, Title
+from users.models import User
 
 from .filters import FilterForTitles
+
 from .serializers import (CategorySerializer, GenreSerializer,
                           ReadOnlyTitleSerializer, TitleSerializer,
-                          ReviewSerializer, CommentSerializer
-                          )
-from .permissions import (RoleAdminrOrReadOnly,
+                          ReviewSerializer, CommentSerializer, UserSerializer,
+                          UserMeSerializer, UserRegistrering,
+                          TokenJWTSerializer)
+from .permissions import (IsAdminUser, RoleAdminrOrReadOnly,
                           IsAdminIsModeratorIsAuthorOrReadOnly)
 
 
@@ -104,3 +117,91 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         review = self.get_review()
         serializer.save(author=self.request.user, review=review)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """Вьюсет для работы с пользователями"""
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'username'
+    permission_classes = (IsAdminUser,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('username',)
+    http_method_names = ('get', 'post', 'head', 'patch', 'delete')
+
+    @action(
+        methods=[
+            "get",
+            "patch",
+        ],
+        detail=False,
+        url_path="me",
+        permission_classes=(IsAuthenticated,),
+        serializer_class=UserMeSerializer,
+    )
+    def profile(self, request):
+        user = request.user
+        if request.method == "GET":
+            serializer = self.get_serializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        if request.method == "PATCH":
+            serializer = self.get_serializer(
+                user,
+                data=request.data,
+                partial=True
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny,])
+def Signup(request):
+    """Регистрация пользователей"""
+
+    serializer = UserRegistrering(data=request.data)
+    if User.objects.filter(
+        username=request.data.get('username'),
+        email=request.data.get('email')
+    ).exists():
+        return Response(request.data, status=status.HTTP_200_OK)
+    if serializer.is_valid():
+        serializer.save()
+        user = get_object_or_404(
+            User, username=serializer.validated_data["username"]
+        )
+        confirmation_code = default_token_generator.make_token(user)
+        send_mail(
+            subject='Код подтверждения',
+            message=f'Код подтверждения для получения '
+                    f'токена {confirmation_code}',
+            from_email=None,
+            recipient_list=[user.email],
+        )
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def GetToken(request):
+    """Получение токена"""
+
+    serializer = TokenJWTSerializer(data=request.data)
+    if serializer.is_valid():
+        user = get_object_or_404(
+            User, username=serializer.validated_data["username"]
+        )
+        if default_token_generator.check_token(
+            user, serializer.validated_data["confirmation_code"]
+        ):
+            token = AccessToken.for_user(user)
+            return Response({'token': str(token)}, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
