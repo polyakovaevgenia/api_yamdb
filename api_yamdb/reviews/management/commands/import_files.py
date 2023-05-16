a5d6ce9ae2b1ae8753f7c643a4d3df4e5ed4f703
Aleksandr <114481791+txtahhh56@users.noreply.github.com>
2023-05-16 21:49:13 +0300

    Update import_files.py
    Переписан скрипт под flake8

--- api_yamdb/reviews/management/commands/import_files.py
+++ api_yamdb/reviews/management/commands/import_files.py
@@ -27,114 +27,104 @@ class Command(BaseCommand):
     help = 'Loads data from csv.'
 
     def handle(self, *args, **options):
+        with open(
+                LIST_MODEL['User']['file'], 'r', encoding='utf-8'
+        ) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                LIST_MODEL['User']['model'].objects.get_or_create(
+                    pk=row['id'],
+                    defaults={
+                        'username': row['username'],
+                        'email': row['email'],
+                        'role': row['role'],
+                        'bio': row['bio'],
+                        'first_name': row['first_name'],
+                        'last_name': row['last_name'],
+                    },
+                )
 
-        for item in LIST_MODEL:
-            if item == 'User':
-                with open(
-                        LIST_MODEL[item]['file'], 'r', encoding='utf-8'
-                ) as csvfile:
-                    reader = csv.DictReader(csvfile)
-                    for row in reader:
-                        LIST_MODEL[item]['model'].objects.get_or_create(
-                            pk=row['id'],
-                            defaults={
-                                'username': row['username'],
-                                'email': row['email'],
-                                'role': row['role'],
-                                'bio': row['bio'],
-                                'first_name': row['first_name'],
-                                'last_name': row['last_name'],
-                            },
-                        )
+        with open(
+                LIST_MODEL['Genre']['file'][0], 'r', encoding='utf-8'
+        ) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                LIST_MODEL['Genre']['model'].objects.get_or_create(
+                    pk=row['id'],
+                    defaults={
+                        'name': row['name'],
+                        'slug': row['slug'],
+                    },
+                )
 
-            if item == 'Genre':
-                with open(
-                        LIST_MODEL[item]['file'][0], 'r', encoding='utf-8'
-                ) as csvfile:
-                    reader = csv.DictReader(csvfile)
-                    for row in reader:
-                        LIST_MODEL[item]['model'].objects.get_or_create(
-                            pk=row['id'],
-                            defaults={
-                                'name': row['name'],
-                                'slug': row['slug'],
-                            },
-                        )
+        with open(
+                LIST_MODEL['Genre']['file'][1], 'r', encoding='utf-8'
+        ) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                try:
+                    LIST_MODEL['Genre']['model'].objects.get(
+                        pk=row['genre_id']).titles.add(row['title_id']
+                                                       )
+                except django.db.utils.IntegrityError:
+                    continue
 
-                with open(
-                        LIST_MODEL[item]['file'][1], 'r', encoding='utf-8'
-                ) as csvfile:
-                    reader = csv.DictReader(csvfile)
-                    for row in reader:
-                        try:
-                            LIST_MODEL[item]['model'].objects.get(
-                                pk=row['genre_id']).titles.add(row['title_id']
-                                                               )
-                        except django.db.utils.IntegrityError:
-                            continue
+        with open(
+                LIST_MODEL['Category']['file'], 'r', encoding='utf-8'
+        ) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                LIST_MODEL['Category']['model'].objects.get_or_create(
+                    pk=row['id'],
+                    defaults={
+                        'name': row['name'],
+                        'slug': row['slug'],
+                    },
+                )
 
-            if item == 'Category':
-                with open(
-                        LIST_MODEL[item]['file'], 'r', encoding='utf-8'
-                ) as csvfile:
-                    reader = csv.DictReader(csvfile)
-                    for row in reader:
-                        LIST_MODEL[item]['model'].objects.get_or_create(
-                            pk=row['id'],
-                            defaults={
-                                'name': row['name'],
-                                'slug': row['slug'],
-                            },
-                        )
+        with open(
+                LIST_MODEL['Title']['file'], 'r', encoding='utf-8'
+        ) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                LIST_MODEL['Title']['model'].objects.get_or_create(
+                    pk=row['id'],
+                    defaults={
+                        'name': row['name'],
+                        'year': row['year'],
+                        'category': Category.objects.get(pk=row['category']),
+                    },
+                )
 
-            if item == 'Title':
-                with open(
-                        LIST_MODEL[item]['file'], 'r', encoding='utf-8'
-                ) as csvfile:
-                    reader = csv.DictReader(csvfile)
-                    for row in reader:
-                        LIST_MODEL[item]['model'].objects.get_or_create(
-                            pk=row['id'],
-                            defaults={
-                                'name': row['name'],
-                                'year': row['year'],
-                                'category': Category.objects.get(
-                                    pk=row['category']
-                                ),
-                            },
-                        )
+        with open(
+                LIST_MODEL['Review']['file'], 'r', encoding='utf-8'
+        ) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                LIST_MODEL['Review']['model'].objects.get_or_create(
+                    pk=row['id'],
+                    defaults={
+                        'text': row['text'],
+                        'author': User.objects.get(pk=row['author']),
+                        'score': row['score'],
+                        'pub_date': row['pub_date'],
+                        'title': Title.objects.get(pk=row['title_id']),
+                    },
+                )
 
-            if item == 'Review':
-                with open(
-                        LIST_MODEL[item]['file'], 'r', encoding='utf-8'
-                ) as csvfile:
-                    reader = csv.DictReader(csvfile)
-                    for row in reader:
-                        LIST_MODEL[item]['model'].objects.get_or_create(
-                            pk=row['id'],
-                            defaults={
-                                'text': row['text'],
-                                'author': User.objects.get(pk=row['author']),
-                                'score': row['score'],
-                                'pub_date': row['pub_date'],
-                                'title': Title.objects.get(pk=row['title_id']),
-                            },
-                        )
-
-            if item == 'Comment':
-                with open(
-                        LIST_MODEL[item]['file'], 'r', encoding='utf-8'
-                ) as csvfile:
-                    reader = csv.DictReader(csvfile)
-                    for row in reader:
-                        LIST_MODEL[item]['model'].objects.get_or_create(
-                            pk=row['id'],
-                            defaults={
-                                'review': Review.objects.get(
-                                    pk=row['review_id']
-                                ),
-                                'author': User.objects.get(pk=row['author']),
-                                'text': row['text'],
-                                'pub_date': row['pub_date'],
-                            },
-                        )
+        with open(
+                LIST_MODEL['Comment']['file'], 'r', encoding='utf-8'
+        ) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                LIST_MODEL['Comment']['model'].objects.get_or_create(
+                    pk=row['id'],
+                    defaults={
+                        'review': Review.objects.get(
+                            pk=row['review_id']
+                        ),
+                        'author': User.objects.get(pk=row['author']),
+                        'text': row['text'],
+                        'pub_date': row['pub_date'],
+                    },
+                )
