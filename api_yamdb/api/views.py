6ace25877c5fc24463115ccdcacc7cddd86ce380
Aleksandr <114481791+txtahhh56@users.noreply.github.com>
2023-05-18 20:11:57 +0300

    Update views.py
    Миксины были перенесены в отдельный файл.

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -5,7 +5,7 @@ from django.core.mail import send_mail
 from django_filters.rest_framework import DjangoFilterBackend
 from django.shortcuts import get_object_or_404
 
-from rest_framework import filters, mixins, viewsets, status, serializers
+from rest_framework import filters, viewsets, status, serializers
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
@@ -16,6 +16,8 @@ from users.models import User
 
 from .filters import FilterForTitles
 
+from .mixins import ListCreateDestroyViewSet
+
 from .serializers import (CategorySerializer, GenreSerializer,
                           ReadOnlyTitleSerializer, TitleSerializer,
                           ReviewSerializer, CommentSerializer, UserSerializer,
@@ -23,15 +25,6 @@ from .serializers import (CategorySerializer, GenreSerializer,
                           TokenJWTSerializer)
 from .permissions import (IsAdminUser, RoleAdminrOrReadOnly,
                           IsAdminIsModeratorIsAuthorOrReadOnly)
-
-
-class ListCreateDestroyViewSet(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    mixins.DestroyModelMixin,
-    viewsets.GenericViewSet
-):
-    pass
 
 
 class CategoryViewSet(ListCreateDestroyViewSet):
