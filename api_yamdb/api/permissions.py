3a404037dbef8936c321b3c11131d21e6c9c95ac
Евгений Чичин <eugenechichin@yandex.ru>
2023-05-20 13:40:03 +0300

    Комментарий к ветке Evgenii2

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,9 +3,10 @@ from rest_framework import permissions
 
 class IsAdminUser(permissions.BasePermission):
     """"Доступ для администратора и суперпользователя """
+
     def has_permission(self, request, view):
         return (request.user.is_authenticated
-                and (request.user.is_admin or request.user.is_superuser)
+                and request.user.is_admin
                 )
 
 
@@ -15,7 +16,7 @@ class RoleAdminrOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.method in permissions.SAFE_METHODS or (
             request.user.is_authenticated
-            and (request.user.is_admin or request.user.is_superuser)
+            and request.user.is_admin
         )
 
 
@@ -30,4 +31,4 @@ class IsAdminIsModeratorIsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or (obj.author == request.user or request.user.is_admin
-                    or request.user.is_moderator or request.user.is_superuser))
+                    or request.user.is_moderator))
